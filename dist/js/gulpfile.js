f08bc712ab95c75bce9e13f7b450588d56f44c57
Ecmascript Hacker <Ecmascript.Hacker@gmail.com>
2016-02-25 09:58:36 +0545

    gulp-uglify example is setup.

---
+++ dist/js/gulpfile.js
@@ -0,0 +1,1 @@
+var gulp=require("gulp"),uglify=require("gulp-uglify");gulp.task("default",function(){console.log("default gulp task is running.")}),gulp.task("compress",function(){return gulp.src("src/js/*.js").pipe(uglify()).pipe(gulp.dest("dist/js"))});
